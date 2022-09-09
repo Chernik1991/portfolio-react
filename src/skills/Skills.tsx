9955fc542c5bd54566a59316e6fa3acb27a5c1c1
MAKS <5059585@mail.ru>
2022-09-09 17:50:03 +0300

    1 Lesson finish, not work

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import s from './Skills.module.css'
 import c from '../../src/common/styles/Container.module.css'
+import {Skill} from './skill/Skill';
+
 export const Skills = () => {
     return (
         <div className={s.skillsBlock}>
             <div className={`${c.container} ${s.skillsContainer}`}>
                 <h2 className={s.title}>Skills</h2>
                 <div className={s.skills}>
-
+                    <Skill title={"JS"} discription={"send......."}/>
+                    <Skill title={"CSS"} discription={"send2......."}/>
+                    <Skill title={"REACT"} discription={"send3......."}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import s from './Skill.module.css'
-import c from '../../src/common/styles/Container.module.css'
-export const Skill = () => {
-    return (
-        <div className={s.skillsBlock}>
-            <div className={`${c.container} ${s.skillsContainer}`}>
-                <h2 className={s.title}>Skills</h2>
-                <div className={s.skills}>
 
-                </div>
-            </div>
+export const Skill = (props:any) => {
+    return (
+        <div className={s.skill}>
+            <div className={s.icon}></div>
+            <h3>{props.title}</h3>
+            <span className={s.discription}>{props.discription}</span>
         </div>
     );
 };
